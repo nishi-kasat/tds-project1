7a3e24f67d3f05a6218ab25c64bee4484623f797
Nishi Kasat <23f3002823@ds.study.iitm.ac.in>
2025-10-15 23:19:25 +0530

    Delete api/test-env.js

--- api/test-env.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  res.status(200).json({
-    secret_seen_by_server: process.env.STUDENT_SECRET || "missing",
-    owner_seen_by_server: process.env.GH_OWNER || "missing"
-  });
-}
